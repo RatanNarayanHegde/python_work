e887c0493d3259dd6dc1a7212ec00e80ce047d13
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-04-12 17:59:23 +0530

    add json read write

--- read_write.py
+++ read_write.py
@@ -1,6 +1,12 @@
-pi = 22/7
-with open('pi_digits.txt','w') as file:
-    file.write(str(pi))
-with open('pi_digits.txt') as file:
-    lines = file.read()
-print(lines[:4])
+import json
+filename = 'username.json'
+try:
+    with open(filename) as file:
+        username = json.load(file)
+except:
+    username = input("Enter your username ")
+    with open(filename,'w') as file:
+        json.dump(username,file)
+    print("We'll Remember you next time!")
+else:
+    print("Welcome "+username+" to the game")   
